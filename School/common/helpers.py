a9365412653e23e55b52df610913e563a3b595cb
tasim313 <mostasimmahmudtasim@gmail.com>
2023-12-08 02:02:01 +0600

    validate school instance

--- School/common/helpers.py
+++ School/common/helpers.py
@@ -5,19 +5,13 @@ logger = logging.getLogger(__name__)
 
 
 def get_school_instance(uid):
-    from django.core.exceptions import (
-        ObjectDoesNotExist,
-        MultipleObjectsReturned)
+    from django.core.exceptions import ObjectDoesNotExist
     from .models import SchoolInformationOnBoarding
+
     try:
         instance = SchoolInformationOnBoarding.objects.get(uid=uid)
         school = instance.id
         return school
-    except MultipleObjectsReturned:
-        instance = SchoolInformationOnBoarding.objects.filter(uid=uid)[0]
-        school = instance.id
-        return school
     except ObjectDoesNotExist:
         logging.error("This school does not exist")
-    isinstance = SchoolInformationOnBoarding.objects.filter(uid=uid)
-    return isinstance.id
+        return None
