682fc3b0fe435f5448d9075cf486f37736b592a4
tasim313 <mostasimmahmudtasim@gmail.com>
2023-12-07 22:28:04 +0600

    auth URL change

--- School/School/Urls/school_auth.py
+++ School/School/Urls/school_auth.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-     path("registration/", include("school_auth.rest.urls")),
+     path("api/", include("school_auth.rest.urls")),
 ]
--- School/school_auth/rest/urls/__init__.py
+++ School/school_auth/rest/urls/__init__.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 
 urlpatterns = [ 
-    path("school/student/", include("school_auth.rest.urls.studentRegister")),
+    path("school/student/registration", include("school_auth.rest.urls.studentRegister")),
     path("login/", include("school_auth.rest.urls.Login")),
-    path("school/admin/", include("school_auth.rest.urls.SchoolAdminRegister")),
+    path("school/admin/registration", include("school_auth.rest.urls.SchoolAdminRegister")),
 ]
