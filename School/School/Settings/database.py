b214a5a34e67052c58f80399c73dd47a5789e4f2
tasim313 <mostasimmahmudtasim@gmail.com>
2023-12-09 21:19:36 +0600

    postgres set up

--- School/School/Settings/database.py
+++ School/School/Settings/database.py
@@ -2,27 +2,12 @@ from .auth import BASE_DIR
 import os
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'my_mysql_database',
-    #     'USER': 'mysql_username',
-    #     'PASSWORD': 'mysql_password',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Tasim',
+        'USER': 'school',
+        'PASSWORD': 'admin*#123',
+        'HOST': 'localhost',  
+        'PORT': '5432',    
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'htwoyext',
-#         'USER': 'htwoyext',
-#         'PASSWORD': 'hba-014tW6JBqhAjjuVagqYg8kxvGnng',
-#         'HOST': 'berry.db.elephantsql.com',
-#         'PORT': 5432,
-#     }
-# }
