c48a3c9632a7f6bcf9458e31676120faca935293
tasim313 <mostasimmahmudtasim@gmail.com>
2023-12-11 21:43:06 +0600

    add cross origin and media file url add docker file and docker-compose yml

--- School/School/Settings/static.py
+++ School/School/Settings/static.py
@@ -8,10 +8,13 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(ROOT_PATH, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_DIR = BASE_DIR / 'media'
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = 'http://195.35.21.202:8000/media/'
 MEDIA_URL_2 = '/media/'
+
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage',
