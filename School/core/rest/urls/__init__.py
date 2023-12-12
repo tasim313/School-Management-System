386f0f32926b72d0c9a95844c25ce394df082175
WazedKhan <wazedkhan119399@gmail.com>
2023-12-12 22:25:04 +0600

    academic list endpoint

--- School/core/rest/urls/__init__.py
+++ School/core/rest/urls/__init__.py
@@ -2,7 +2,20 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("school/information/", include("core.rest.urls.websiteInfo")),
-    path('slider/content/', include("core.rest.urls.sliderContent")),
-    path("school/admission/", include("core.rest.urls.admission")),
+    path(
+        "school/information/",
+        include("core.rest.urls.websiteInfo"),
+    ),
+    path(
+        "school/admission/",
+        include("core.rest.urls.admission"),
+    ),
+    path(
+        "school/academic-information/",
+        include("core.rest.urls.academic"),
+    ),
+    path(
+        "slider/content/",
+        include("core.rest.urls.sliderContent"),
+    ),
 ]
