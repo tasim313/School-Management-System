b295fe856feb8015dccc1a75e3372c0b1280cd6e
WazedKhan <wazedkhan119399@gmail.com>
2023-12-14 21:49:44 +0600

    fixed code format

--- School/core/rest/views/websiteInfo.py
+++ School/core/rest/views/websiteInfo.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication 
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import status, serializers
 from rest_framework.generics import RetrieveAPIView
 from django.shortcuts import get_object_or_404
@@ -11,7 +11,7 @@ from ..serializers import websiteInfo
 from ...models import (
     WebsiteInformation,
     SchoolAddressInformation,
-    SchoolContactInformation
+    SchoolContactInformation,
 )
 
 import logging
@@ -26,25 +26,26 @@ class SchoolWebsiteAPIView(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        
+
         try:
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
 
             response_data = {
-                'message': 'School Website Information Create successful.',
-                'data': serializer.data
+                "message": "School Website Information Create successful.",
+                "data": serializer.data,
             }
-            return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+            return Response(
+                response_data, status=status.HTTP_201_CREATED, headers=headers
+            )
         except serializers.ValidationError as e:
             error_data = {
-                'message': 'School Website Information failed.',
-                'errors': e.detail
+                "message": "School Website Information failed.",
+                "errors": e.detail,
             }
             return Response(error_data, status=status.HTTP_400_BAD_REQUEST)
-        
-        
+
 
 class WebsiteInformationListView(generics.ListAPIView):
     queryset = WebsiteInformation.objects.all()
@@ -55,19 +56,21 @@ class WebsiteInformationListView(generics.ListAPIView):
         return self.queryset.filter(school_website__slug=school_slug)
 
 
-
 class SchoolWebsiteUpdateAPIView(generics.UpdateAPIView):
     serializer_class = websiteInfo.SchoolWebsiteUpdateSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-    lookup_field = 'uid'
-    allowed_methods = ['PUT', 'PATCH',]
+    lookup_field = "uid"
+    allowed_methods = [
+        "PUT",
+        "PATCH",
+    ]
 
     def get_queryset(self):
-        return WebsiteInformation.objects.all() 
+        return WebsiteInformation.objects.all()
 
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+        partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
 
@@ -76,14 +79,14 @@ class SchoolWebsiteUpdateAPIView(generics.UpdateAPIView):
             self.perform_update(serializer)
 
             response_data = {
-                'message': 'School Website Information update successful.',
-                'data': serializer.data
+                "message": "School Website Information update successful.",
+                "data": serializer.data,
             }
             return Response(response_data, status=status.HTTP_200_OK)
         except serializers.ValidationError as e:
             error_data = {
-                'message': 'School Website Information update failed.',
-                'errors': e.detail
+                "message": "School Website Information update failed.",
+                "errors": e.detail,
             }
             return Response(error_data, status=status.HTTP_400_BAD_REQUEST)
 
