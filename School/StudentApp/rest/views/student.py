02d30c6a0aa4ff5ab00e68f8042672717e62d9c3
WazedKhan <wazedkhan119399@gmail.com>
2023-12-31 21:35:00 +0600

    student detail endpoint with api collection

--- School/StudentApp/rest/views/student.py
+++ School/StudentApp/rest/views/student.py
@@ -1,14 +1,15 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication 
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import status, serializers
 
+from common.custom_views import CustomRetrieveUpdateDestroyAPIView
+from common.choice import Status
 
 from ..serializers import student
 
 from ...models import Student
-
 
 
 class StudentInformationListView(generics.ListAPIView):
@@ -19,7 +20,23 @@ class StudentInformationListView(generics.ListAPIView):
     def get_queryset(self):
         school_slug = self.kwargs.get("school_slug", None)
         queryset = Student.objects.filter(
-            school_student__slug=school_slug
-        ).select_related('school_student')
+            status=Status.Active,
+            school_student__slug=school_slug,
+        ).select_related("school_student")
 
         return queryset
+
+
+class StudentDetail(CustomRetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = student.StudentInformationListSerializer
+    lookup_field = "slug"
+
+    def get_queryset(self):
+        school_id = self.request.user.school_id
+        student = Student.objects.filter(
+            status=Status.Active,
+            school_student_id=school_id,
+        )
+        return student
---
+++ School/common/custom_views.py
@@ -0,0 +1,58 @@
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework import status
+from rest_framework.response import Response
+
+
+from core.choice import Status
+
+
+class CustomRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    Custom view for retrieving, updating, and deleting an object.
+    """
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Handle DELETE request.
+
+        This method is responsible for deleting the object.
+        If the instance's status is to be set as 'Inactive', it updates the status.
+        Otherwise, it performs the regular deletion.
+        """
+
+        # Get the object instance
+        instance = self.get_object()
+
+        try:
+            # Attempt to update the status to 'Inactive'
+            instance.status = Status.Inactive
+            instance.save()  # Save the changes to the instance
+            return Response(
+                {"message": "Successful!"}, status=status.HTTP_204_NO_CONTENT
+            )  # Return success response for 'Inactive' update
+        except Exception as e:
+            # If there's an exception (e.g., status field not available or saving fails), perform regular deletion
+            self.perform_destroy(instance)
+            return Response(
+                {"message": "Successful!"}, status=status.HTTP_204_NO_CONTENT
+            )
+
+    def update(self, request, *args, **kwargs):
+        """
+        Handle PUT request for updating the object.
+        """
+
+        # Call the parent class method to perform the update
+        super().update(request, *args, **kwargs)
+
+        # Get the updated instance
+        updated_instance = self.get_object()
+
+        # Serialize the updated instance
+        serialized_data = self.get_serializer(updated_instance).data
+
+        # Return a response with success message and serialized data
+        return Response(
+            {"message": "Successful!", "data": serialized_data},
+            status=status.HTTP_200_OK,
+        )
