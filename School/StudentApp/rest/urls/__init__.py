2240429deac76783dd68332e06e030ec94f42dc6
WazedKhan <wazedkhan119399@gmail.com>
2023-12-31 21:35:00 +0600

    student detail endpoint with api collection

--- School/StudentApp/rest/urls/__init__.py
+++ School/StudentApp/rest/urls/__init__.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path(
-        "information/",
+        "",
         include("StudentApp.rest.urls.student"),
     ),
 ]
--- School/StudentApp/rest/urls/student.py
+++ School/StudentApp/rest/urls/student.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from StudentApp.rest.views.student import (
     StudentInformationListView,
+    StudentDetail,
 )
 
 
 urlpatterns = [
-    path('<slug:school_slug>/', 
-         StudentInformationListView.as_view(),
-         name='student-information-list'),
+    path(
+        "information/<slug:school_slug>/",
+        StudentInformationListView.as_view(),
+        name="student-information-list",
+    ),
+    path(
+        "<slug:slug>/",
+        StudentDetail.as_view(),
+        name="student-details",
+    ),
 ]
