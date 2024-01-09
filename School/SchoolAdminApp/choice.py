d0fa67011d1ec08e381303a2023e6229c850a8c8
tasim313 <mostasimmahmudtasim@gmail.com>
2024-01-10 00:12:37 +0600

    create model forCareer, Department

--- School/SchoolAdminApp/choice.py
+++ School/SchoolAdminApp/choice.py
@@ -5,3 +5,20 @@ class Gender(models.TextChoices):
     FEMALE = "female", "Female"
     MALE = "male", "Male"
     OTHERS = "others", "Others"
+
+
+
+class JobType(models.TextChoices):
+    FullTime = 'full_time', 'Full Time'
+    PartTime = 'part_time', 'Part Time'
+    Internship = 'internship', 'Internship'
+    Temporary = 'temporary', 'Temporary'
+    Remote = 'remote', 'Remote'
+    Other = 'other', "Other"
+
+
+
+class JobStatus(models.TextChoices):
+    Open = 'open', 'Open'
+    Closed = 'closed', 'Closed'
+    Cancelled = 'cancelled', 'Cancelled'
