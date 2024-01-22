10e17f474621e0c81961c0338ef5579327b286ce
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2024-01-22 21:29:40 +0600

    transport serializer updated

--- School/SchoolAdminApp/rest/serializers/transport.py
+++ School/SchoolAdminApp/rest/serializers/transport.py
@@ -27,3 +27,21 @@ class TransportListSerializer(serializers.ModelSerializer):
             "driver_address",
         ]
         read_only_fields = ["uid", "slug"]
+
+    def create(self, validated_data):
+        instance = super().create(validated_data=validated_data)
+
+        # Add user_created by request user
+        instance.user_created = self.context["request"].user
+        instance.save(update_fields=["user_created"])
+
+        return instance
+
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data=validated_data)
+
+        # Add user_updated by request user
+        instance.user_updated = self.context["request"].user
+        instance.save(update_fields=["user_updated"])
+
+        return instance
