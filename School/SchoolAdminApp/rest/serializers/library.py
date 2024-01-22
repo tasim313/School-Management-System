888cf165233ac3ef550e4b1b647c9b94d95f0332
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2024-01-22 21:38:14 +0600

    library serializer updated.

--- School/SchoolAdminApp/rest/serializers/library.py
+++ School/SchoolAdminApp/rest/serializers/library.py
@@ -28,3 +28,21 @@ class LibraryListSerializer(serializers.ModelSerializer):
             "book_status",
         ]
         read_only_fields = ["uid", "slug"]
+
+    def create(self, validated_data):
+        instance = super().create(validated_data=validated_data)
+
+        # Add user_created by request user
+        instance.user_created = self.context["request"].user
+        instance.save(update_fields=["user_created"])
+
+        return instance
+
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data=validated_data)
+
+        # Add user_updated by request user
+        instance.user_updated = self.context["request"].user
+        instance.save(update_fields=["user_updated"])
+
+        return instance
