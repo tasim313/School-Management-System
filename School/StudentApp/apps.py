698f2702e90abb0bd43cff5a1fd4e41dfd8cc64e
WazedKhan <wazedkhan119399@gmail.com>
2024-01-23 23:30:07 +0600

    update total student in class model

--- School/StudentApp/apps.py
+++ School/StudentApp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StudentappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "StudentApp"
+
+    def ready(self):
+        import StudentApp.signals
