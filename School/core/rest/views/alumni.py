3f20acd9ea2364f291bb3572afce89ea04cfc563
tasim313 <mostasimmahmudtasim@gmail.com>
2024-01-30 21:48:18 +0600

    alumni api

---
+++ School/core/rest/views/alumni.py
@@ -0,0 +1,115 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from core.models import AlumniSection, AlumniSectionImage
+from core.rest.serializers.alumni import AlumniSectionListSerializer, AlumniSectionImageListSerializer
+
+from common.choice import Status
+
+
+class AlumniSectionListCreateView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = AlumniSectionListSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsAuthenticated()]
+        else:
+            return []
+
+    def get_queryset(self):
+        school_slug = self.kwargs.get("school_slug", None)
+
+        queryset = AlumniSection.objects.filter(
+            status=Status.Active,
+            school_alumni_section__slug=school_slug,
+        ).select_related(
+            "school_alumni_section"
+        )
+
+        return queryset
+
+
+class AlumniSectionRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = AlumniSectionListSerializer
+    lookup_field = "uid"
+
+    def get_permissions(self):
+        if (
+                self.request.method == "PUT" or
+                self.request.method == "PATCH" or
+                self.request.method == "DELETE"
+        ):
+            return [IsAuthenticated()]
+        else:
+            return []
+
+    def get_queryset(self):
+       
+        school_slug = self.kwargs.get("school_slug", None)
+
+        school_alumni_section = AlumniSection.objects.filter(
+            status=Status.Active,
+            school_alumni_section__slug=school_slug,
+        ).select_related(
+            "school_alumni_section"
+        )
+
+        return school_alumni_section
+
+
+
+class AlumniSectionImageListCreateView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = AlumniSectionImageListSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [IsAuthenticated()]
+        else:
+            return []
+
+    def get_queryset(self):
+        school_slug = self.kwargs.get("school_slug", None)
+
+        queryset = AlumniSectionImage.objects.filter(
+            status=Status.Active,
+            alumni_info__school_alumni_section__slug=school_slug,
+        ).select_related(
+            "alumni_info"
+        )
+
+        return queryset
+
+
+class AlumniSectionImageRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = AlumniSectionImageListSerializer
+    lookup_field = "uid"
+
+    def get_permissions(self):
+        if (
+                self.request.method == "PUT" or
+                self.request.method == "PATCH" or
+                self.request.method == "DELETE"
+        ):
+            return [IsAuthenticated()]
+        else:
+            return []
+
+    def get_queryset(self):
+       
+        school_slug = self.kwargs.get("school_slug", None)
+
+        alumni_info = AlumniSectionImage.objects.filter(
+            status=Status.Active,
+            alumni_info__school_alumni_section__slug=school_slug,
+        ).select_related(
+            "alumni_info"
+        )
+
+        return alumni_info
