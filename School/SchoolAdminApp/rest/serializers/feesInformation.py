8fc59c93b5c33238d8121ebc7618c6725494f276
tasim313 <mostasimmahmudtasim@gmail.com>
2024-01-30 20:58:49 +0600

    fees api

---
+++ School/SchoolAdminApp/rest/serializers/feesInformation.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from SchoolAdminApp.models import FeesInformation, FeesCategory
+from core.models import SchoolClass, SchoolSection
+
+
+
+class FeesInformationSerializer(serializers.ModelSerializer):
+    
+    fees_category = serializers.SlugRelatedField(
+        queryset=FeesCategory.objects.all(),
+        slug_field="uid",
+    )
+    fess_class = serializers.SlugRelatedField(
+        queryset=SchoolClass.objects.all(),
+        slug_field="uid",
+    )
+    fess_section = serializers.SlugRelatedField(
+        queryset=SchoolSection.objects.all(),
+        slug_field="uid",
+    )
+
+    class Meta:
+        model = FeesInformation
+        fields = [
+            "uid",
+            "fees_amount",
+            "fees_start",
+            "fees_end",
+            'fees_category',
+            'fess_class',
+            'fess_section'
+        ]
+        read_only_fields = ["uid"]
+
+    def create(self, validated_data):
+        instance = super().create(validated_data=validated_data)
+        instance.user_created = self.context["request"].user
+        instance.save(update_fields=["user_created"])
+
+        return instance
+
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data=validated_data)
+        instance.user_updated = self.context["request"].user
+        instance.save(update_fields=["user_updated"])
+
+        return instance
