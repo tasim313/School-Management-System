783ecce631499c5751cd517ea1d46541052c71a0
WazedKhan <wazedkhan119399@gmail.com>
2024-03-03 22:22:24 +0600

    attendance update delete

--- School/core/rest/urls/attendance.py
+++ School/core/rest/urls/attendance.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from core.rest.views.attendance import ClassAttendanceListCreate
+from core.rest.views.attendance import ClassAttendanceListCreate, ClassAttendanceDetail
 
 urlpatterns = [
     path(
         "",
         ClassAttendanceListCreate.as_view(),
         name="class-attendance-list-create",
+    ),
+    path(
+        "<uuid:uid>/",
+        ClassAttendanceDetail.as_view(),
+        name="class-attendance-detail",
     ),
 ]
