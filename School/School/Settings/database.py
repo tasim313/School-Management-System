1fd8d04ef453ac4cf1b1578b8e33f242455728ec
root <root@ubuntu-cloudpanel.localhost>
2024-03-15 15:32:20 +0000

    changes database and docker file

--- School/School/Settings/database.py
+++ School/School/Settings/database.py
@@ -2,16 +2,16 @@ from .auth import BASE_DIR
 import os
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'postgresql': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'project101',        
         'USER': 'tasim',           
         'PASSWORD': 'tasim*#2024',    
-        'HOST': 'postgres',           
+        'HOST': 'postgres_1',           
         'PORT': '5432',               
     }
 }
