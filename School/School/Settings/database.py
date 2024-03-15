9ea197a759cb1c1f8291a18edf3bb2e632ef07af
root <root@ubuntu-cloudpanel.localhost>
2024-03-15 09:32:27 +0000

    docker-update

--- School/School/Settings/database.py
+++ School/School/Settings/database.py
@@ -2,27 +2,16 @@ from .auth import BASE_DIR
 import os
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'my_mysql_database',
-    #     'USER': 'mysql_username',
-    #     'PASSWORD': 'mysql_password',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    # },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgresql': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'project101',        
+        'USER': 'tasim',           
+        'PASSWORD': 'tasim*#2024',    
+        'HOST': 'postgres',           
+        'PORT': '5432',               
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'htwoyext',
-#         'USER': 'htwoyext',
-#         'PASSWORD': 'hba-014tW6JBqhAjjuVagqYg8kxvGnng',
-#         'HOST': 'berry.db.elephantsql.com',
-#         'PORT': 5432,
-#     }
-# }
