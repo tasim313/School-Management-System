262101e93bcd2b8a7828f6f9b0d8998db8cb4333
WazedKhan <wazedkhan119399@gmail.com>
2024-03-28 22:20:48 +0600

    environment based db configuration

--- School/School/Settings/database.py
+++ School/School/Settings/database.py
@@ -1,17 +1,27 @@
-from .auth import BASE_DIR
 import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env
+load_dotenv()
+
+# Define BASE_DIR
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Common database settings
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project101',        
-        'USER': 'tasim',           
-        'PASSWORD': 'tasim*#2024',    
-        'HOST': 'postgres_1',           
-        'PORT': '5432',               
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.path.join(BASE_DIR, os.getenv("DB_NAME")),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
+
+# Override DATABASES if using SQLite3 for development
+if os.getenv("DB_ENGINE") == "django.db.backends.sqlite3":
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, os.getenv("DB_NAME")),
+    }
