b440eadbea7b7f54a32ecfc20ef0c2a3e6c8f9cb
tasim313 <mostasimmahmudtasim@gmail.com>
2024-04-28 20:07:26 +0600

    student current status module

---
+++ School/StudentApp/rest/urls/student_current_status.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from StudentApp.rest.views.student_current_status import (
+    StudentCurrentStatusListCreateView,
+    StudentCurrentStatusRetrieveUpdateDeleteView
+)
+
+
+urlpatterns = [
+    path(
+        "<slug:school_slug>/",
+        StudentCurrentStatusListCreateView.as_view(),
+        name="student-status-list",
+    ),
+    path(
+        "<slug:slug>/<uuid:uid>/",
+        StudentCurrentStatusRetrieveUpdateDeleteView.as_view(),
+        name="student-status-details",
+    ),
+]
