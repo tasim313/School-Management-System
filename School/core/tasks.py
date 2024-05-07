7efe643412dbf5148292b8d2d5825b3a6aa7bfaf
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2024-05-07 22:32:18 +0600

    create_student_attendance_record final

--- School/core/tasks.py
+++ School/core/tasks.py
@@ -1,12 +1,15 @@
 import logging
+
 from tqdm import tqdm
-from django.utils import timezone
 
 from celery import shared_task
 
-from common.models import SchoolInformationOnBoarding
-from core.models import ClassAttendance
+from django.utils import timezone
+
 from StudentApp.models import StudentCurrentStatus
+
+from core.choice import Status
+from core.models import ClassAttendance
 
 logger = logging.getLogger(__name__)
 
@@ -15,30 +18,24 @@ def create_student_attendance_record():
     date = timezone.now().date()
     logger.info(f"Start: Creating attendance record for students on {date}")
 
-    print("Creating attendance record for students")
+    students_current_status = StudentCurrentStatus.objects.filter(
+        status=Status.Active
+    ).select_related(
+        "student_current_status",
+        "current_class",
+        "current_section",
+    )
 
-    school_id_list = SchoolInformationOnBoarding.objects.values_list("id", flat=True)
-
-    for school_id in tqdm(school_id_list):
-        students_current_status = StudentCurrentStatus.objects.filter(
-            student_current_status__school_student_id=school_id,
-        ).select_related(
-            "student_current_status",
-            "current_class",
-            "current_section",
+    for student_current_status in students_current_status:
+        # Create an instance of ClassAttendance for each student
+        attendance_record = ClassAttendance(
+            attendance_class_id=student_current_status.current_class.id,
+            attendance_section_id=student_current_status.current_section.id,
+            attendance_student_id=student_current_status.student_current_status.id,
+            school_id=student_current_status.student_current_status.school_student.id,
+            date=date,
         )
-
-        for student_current_status in students_current_status:
-            # Create an instance of ClassAttendance for each student
-            attendance_record = ClassAttendance(
-                attendance_class_id=student_current_status.current_class.id,
-                attendance_section_id=student_current_status.current_section.id,
-                attendance_student_id=student_current_status.student_current_status.id,
-                school_id=student_current_status.student_current_status.school_student.id,
-                date=date,
-            )
-            # Save the instance to the database
-            attendance_record.save()
+        # Save the instance to the database
+        attendance_record.save()
 
     logger.info(f"End: Attendance record created for students on {date}")
-
