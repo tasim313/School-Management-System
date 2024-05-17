b7f8616696793ccc90ebb9df6be4facdc77594fa
root <mostasimmahmudtasim@gmail.com>
2024-05-17 13:32:18 +0000

    chnages media url and debug mode

--- School/School/Settings/static.py
+++ School/School/Settings/static.py
@@ -11,5 +11,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = "https://core-sm.online/media/"
+MEDIA_URL = "/media/"
 MEDIA_URL_2 = "/media/"
